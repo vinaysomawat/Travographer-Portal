05f1b8bd99ef03341a59f2d4516afbd455c324f6
Vinay Somawat <svinay1@student.nitw.ac.in>
2020-04-21 16:16:29 +0530

    upload file option added

--- uploadportal/urls.py
+++ uploadportal/urls.py
@@ -4,4 +4,5 @@ from django.views.generic.base import TemplateView
 
 urlpatterns = [
     path('', TemplateView.as_view(template_name='home.html'), name='home'),
+    path('upload/', views.upload, name='upload'),
 ]
--- uploadportal/views.py
+++ uploadportal/views.py
@@ -1,8 +1,17 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView, ListView, CreateView
+from django.core.files.storage import FileSystemStorage
 
 # Create your views here.
 
 class Home(TemplateView):
     template_name = 'home.html'
 
+def upload(request):
+    context = {}
+    if request.method == 'POST':
+        uploaded_file = request.FILES['document']
+        fs = FileSystemStorage()
+        name = fs.save(uploaded_file.name, uploaded_file)
+        context['url'] = fs.url(name)
+    return render(request, 'upload.html', context)
